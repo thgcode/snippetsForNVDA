f1f9e6f60065dcf3ff1e347aeb262ae59ba43226
Thiago Seus <thiago.seus@yahoo.com.br>
2020-06-17 11:11:41 -0300

    Rename the script names to be more obvious and fix the bug that it wasn't speaking when the clipboard is empty on the save to memory script

--- scratchpad/globalPlugins/memory.py
+++ scratchpad/globalPlugins/memory.py
@@ -5,16 +5,20 @@ import ui
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     memory = {}
 
-    def script_registerToMemory(self, gesture):
+    def script_saveToMemory(self, gesture):
         """When pressed, this key saves the text copied to the clipboard to this position on the NVDA memory."""
-        clipboardContent = api.getClipData()
+        try:
+            clipboardContent = api.getClipData()
+        except OSError:
+            ui.message("Clipboard is empty")
+            return
         keyCode = gesture.vkCode # Get which number the user pressed.
         # The number will be in the range 48 to 57 inclusive, and we will use it to hash the data in the dictionary.
         if isinstance(clipboardContent, str):
             self.memory[keyCode] = clipboardContent
             ui.message("Saved")
 
-    def script_speakMemory(self, gesture):
+    def script_speakAndCopyMemory(self, gesture):
         """When pressed, this key speaks and copies the text saved at this position of the NVDA memory to the clipboard."""
         keyCode = gesture.vkCode
         try:
@@ -29,5 +33,5 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     # Maps all 10 numeric keyboard keys to the apropriate gesture.
     # It was done this way to avoid code repetition and to facilitate adding more commands in the future.
     for keyboardKey in range(11):
-            __gestures[f"kb:NVDA+CONTROL+{keyboardKey}"] = "registerToMemory"
-            __gestures[f"kb:NVDA+CONTROL+SHIFT+{keyboardKey}"] = "speakMemory"
+            __gestures[f"kb:NVDA+CONTROL+{keyboardKey}"] = "saveToMemory"
+            __gestures[f"kb:NVDA+CONTROL+SHIFT+{keyboardKey}"] = "speakAndCopyMemory"
