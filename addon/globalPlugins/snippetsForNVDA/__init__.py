6ed9a02465eb18b5e1a5cba3deaea1f58271c081
Thiago Seus <thiago.seus@yahoo.com.br>
2020-06-23 10:00:02 -0300

    Add the translation system

--- addon/globalPlugins/snippetsForNVDA/__init__.py
+++ addon/globalPlugins/snippetsForNVDA/__init__.py
@@ -1,3 +1,4 @@
+import addonHandler
 import api
 import controlTypes
 import globalPluginHandler
@@ -6,12 +7,18 @@ import textInfos
 import ui
 from scriptHandler import getLastScriptRepeatCount
 
+# Save the NVDA translation function so that we can use it if we need it
+nvdaTranslation = _
+
+# Then, init our translation system
+# At this point, the _ (underscore) function will be rebound with the translations of our addon
+addonHandler.initTranslation()
+
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     memory = {}
     lastPressedKey = 0
 
     def script_saveToMemory(self, gesture):
-        """When pressed, this key saves the selected text to this position on the NVDA memory."""
         focus = api.getFocusObject()
         textInfo = None
         if focus.windowClassName in ["AkelEditW"] or focus.role in [controlTypes.ROLE_EDITABLETEXT]:
@@ -24,12 +31,15 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
                 keyCode = gesture.vkCode # Get which number the user pressed.
                 # The number will be in the range 48 to 57 inclusive, and we will use it to hash the data in the dictionary.
                 self.memory[keyCode] = text
-                ui.message("Saved")
+                # Translators: This message is displayed when text is saved on a memory slot.
+                ui.message(_("Saved"))
             else:
-                ui.message("No selection")
+                ui.message(nvdaTranslation("No selection"))
+
+    # Translators: the documentation of the save to memory slot command, displayed on the input help mode.
+    script_saveToMemory.__doc__ = _("""When pressed, this key saves the selected text to this memory slot.""")
 
     def script_speakAndCopyMemory(self, gesture):
-        """When pressed, this key speaks and copies the text saved at this position of the NVDA memory to the clipboard."""
         keyCode = gesture.vkCode
         try:
             data = self.memory[keyCode]
@@ -38,7 +48,8 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
                 self.lastPressedKey = keyCode
             elif getLastScriptRepeatCount() == 1 and self.isLastPressedKey(keyCode):
                 api.copyToClip(data)
-                ui.message(f"Copied {data}")
+                # Translators: The message displayed when the user pasted this memory slot to an edit field.
+                ui.message(_("Pasted {data}").format(data=data))
                 self.lastPressedKey = 0
                 # Paste the selected text
                 keyboardHandler.KeyboardInputGesture.fromName("CONTROL+V").send()
@@ -46,8 +57,13 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
                 self.lastPressedKey = 0
                 ui.message(data)
         except KeyError:
-            ui.message("No data at this position")
+            # Translators: The message when the user checks a memory slot but there is no data in it
+            ui.message(_("No data at this position"))
             self.lastPressedKey = 0
+
+    # Translators: the documentation of the speak and copy memory slot command, displayed on the input help mode.
+    script_speakAndCopyMemory.__doc__ = _("""Pressing this key combination once , the content of this memory slot will be spoken.
+Pressing it twice in a row, the content of this memory slot will be pasted to the running application.""")
 
     def isLastPressedKey(self, keyCode):
         return self.lastPressedKey == keyCode
